8f2f1fc85cffb5d1fe4cb07d13620c2260ca3fee
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2022-08-11 19:56:00 +0200

    11-08-2022

---
+++ robotica-mobile/+path_planning/+voronoi_diagrams/voronoi.m
@@ -0,0 +1,9 @@
+% 1) [vx,vy]=voronoi
+% 2) ripulire [vx,vy] da ostacoli noti
+% v = []
+% 3) costruisci il grafo a partire dai nodi tramite matrice di adiacenza
+% funzione graph MATLAB
+% 4 opzionale) applichi algoritmo percorso minimo sul grafo per ottenere il
+%              percorso del robot (dijkstra, ...)
+
+
