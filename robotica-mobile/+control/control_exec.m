86977cc51d89e68e2e7bb740a64db08f18ae4190
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2022-08-25 01:12:58 +0200

    24-08-2022

--- robotica-mobile/+control/control_exec.m
+++ robotica-mobile/+control/control_exec.m
@@ -24,7 +24,8 @@ function control_exec(environment, P, Ts, samples)
     end
     
     % PLOT STATICO
-    figure(); plot2D(environment);
+    f=figure(); f.Position=[25 342 1500 420]; subplot(1,3,1);
+    plot2D(environment);
     hold on; plot(evolutionAL(:,1), evolutionAL(:,2));
     title("CONTROL BASED ON APPROXIMATE LINEARIZATION"); 
     legend("start", "goal", "trajectory output with control","Location","northwest");
@@ -56,7 +57,8 @@ function control_exec(environment, P, Ts, samples)
     end
     
     % PLOT STATICO
-    figure(); plot2D(environment);
+    subplot(1,3,2);
+    plot2D(environment);
     hold on; plot(evolutionNL(:,1), evolutionNL(:,2));
     title("NON LINEAR CONTROL"); 
     legend("start", "goal", "trajectory output with control","Location","northwest");
@@ -86,7 +88,8 @@ function control_exec(environment, P, Ts, samples)
     end
     
      % PLOT STATICO
-    figure(); plot2D(environment);
+    subplot(1,3,3);
+    plot2D(environment);
     hold on; plot(evolutionFL(:,1), evolutionFL(:,2));
     title("INPUT-OUTPUT LINEARIZATION"); 
     legend("start", "goal", "trajectory output with control","Location","northwest");
--- robotica-mobile/+path_planning/+discrete_potential_fields/discrete_potential_fields.m
+++ robotica-mobile/+path_planning/+discrete_potential_fields/discrete_potential_fields.m
@@ -7,21 +7,21 @@ function trajectory = discrete_potential_fields(start, goal, width, height, grid
     %percorso minimo
     import path_planning.discrete_potential_fields.search_path;
     [trajectory, dpf_map] = search_path(start(1),start(2), discrete_potential_map);
-    figure();
-    %surface(path_map);
-    plot(trajectory(:,1), trajectory(:,2));
-    hold on;
-    for j = 1 : size(obstacles)
-        ob = obstacles(j,:);
-        x_ob = ob(1);
-        y_ob = ob(3);
-        w_ob = ob(2)-ob(1);
-        h_ob = ob(4)-ob(3);
-        rectangle("position",[x_ob y_ob w_ob h_ob], "facecolor","r");
-    end
-    hold on; plot(start(1), start(2), "*", "Color","#124ee6");
-    hold on; plot(goal(1)+1, goal(2)+1, "*", "Color","#0eb04f");
-    title("DISCRETE POTENTIAL FIELDS WITH ASSOCIATED SHORTEST PATH");
+%     figure();
+%     %surface(path_map);
+%     plot(trajectory(:,1), trajectory(:,2));
+%     hold on;
+%     for j = 1 : size(obstacles)
+%         ob = obstacles(j,:);
+%         x_ob = ob(1);
+%         y_ob = ob(3);
+%         w_ob = ob(2)-ob(1);
+%         h_ob = ob(4)-ob(3);
+%         rectangle("position",[x_ob y_ob w_ob h_ob], "facecolor","r");
+%     end
+%     hold on; plot(start(1), start(2), "*", "Color","#124ee6");
+%     hold on; plot(goal(1)+1, goal(2)+1, "*", "Color","#0eb04f");
+%     title("DISCRETE POTENTIAL FIELDS WITH ASSOCIATED SHORTEST PATH");
 
 
 end
