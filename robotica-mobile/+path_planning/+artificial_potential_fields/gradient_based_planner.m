31db79130aefba75729d0167012f0ad42f7353b6
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2022-08-31 00:22:19 +0200

    30-08-2022

--- robotica-mobile/+path_planning/+artificial_potential_fields/gradient_based_planner.m
+++ robotica-mobile/+path_planning/+artificial_potential_fields/gradient_based_planner.m
@@ -17,31 +17,35 @@
     % FX = gradiente(F) restituisce il gradiente numerico unidimensionale 
     % del vettore F. L'output FX corrisponde a ∂F/∂x, che sono le 
     % differenze nella direzione x (orizzontale). L'uscita aggiuntiva FY 
-    % corrisponde a ∂F/∂y, che sono le differenze di y Direzione 
-    % (verticale). La spaziatura tra i punti in ciascuna direzione Si 
-    % presume che la sia 1.
+    % corrisponde a ∂F/∂y, che sono le differenze nella direzione y 
+    % (verticale). La spaziatura tra i punti in ciascuna direzione si
+    % presume che sia 1.
     [gx, gy] = gradient (-f);
     
+ 
+    route = start_coords;
+    point_on_route = start_coords;
+    %un valore grande permette di arrivare velocemente al goal ma può 
+    % determinare anche per esempio degli overshoot
+    speed = 3; 
+    %è una misura della soglia: se è troppo piccola è accurata ma la 
+    % soluzione è lenta
+    tolerance=1; 
 
-    route=start_coords;
-    Point_on_route=start_coords;
-    Speed=3; %un valore grande permette di arrivare velocemente al goal ma 
-    % può determinare anche per esempio degli overshoot
-    Tolerance=1; %è una misura della soglia, se è troppo piccola è accurata
-    % ma la soluzione è lenta
-
-    while(max_its > 0)
-        % controllo il robot e' arrivato al goal
-        if(norm(end_coords-Point_on_route) < Tolerance)
-            % interrompo il ciclo perche' il robot e' arrivato
+    
+    while( max_its > 0 )
+        
+        % controllo che il robot sia arrivato al goal
+        if(norm(end_coords-point_on_route) < tolerance)
+            % interrompo il ciclo perche' il robot e' arrivato al goal
             break;
         end
-        % 
+        
         % equivalente a scrivere:
         % delta_x = gx(floor(Point_on_route));
         % delta_y = gy(floor(Point_on_route));
-        delta_x = gx(floor(Point_on_route(2)), floor(Point_on_route(1)));
-        delta_y = gy(floor(Point_on_route(2)), floor(Point_on_route(1)));
+        delta_x = gx(floor(point_on_route(2)), floor(point_on_route(1)));
+        delta_y = gy(floor(point_on_route(2)), floor(point_on_route(1)));
         % delta è un vettore sia di valore che direzione
         delta=[delta_x, delta_y];
         % otteniamo la direzione
@@ -60,48 +64,19 @@
         delta_direction_y = delta_y/norm(delta);
 
         % new_point = previous_point + d/dt(previuos_point)*delta_shift
-        new_route_x = Point_on_route(1)+Speed*delta_direction_x;
-        new_route_y = Point_on_route(2)+Speed*delta_direction_y;
+        new_route_x = point_on_route(1)+speed*delta_direction_x;
+        new_route_y = point_on_route(2)+speed*delta_direction_y;
 
         % new_point
-        Point_on_route = [new_route_x, new_route_y];
+        point_on_route = [new_route_x, new_route_y];
 
         % concateno il cammino precedente con il nuovo punto trovato
-        route = [route; Point_on_route];
+        route = [route; point_on_route];
         
         % decremento il numero delle iterazioni da eseguire
         max_its=max_its-1;
 
     end
 
-
-
-
-
-
-
-
-    % route = [start_coords];
-
-    % for i=1:max_its
-    %     current_point = route(end,:);
-    %      if sum( abs(current_point-end_coords) ) < 5.0
-    %          disp('Reached the goal !');
-    %          break
-    %      end
-    %     ix = round( current_point(2) ); % X and Y axis are swaped
-    %     iy = round( current_point(1) );
-    %     w = 10;
-    %     vx = mean( mean( gx(ix-w/2:ix+w/2, iy-w/2:iy+w/2) ) );
-    %     vy = mean( mean( gy(ix-w/2:ix+w/2, iy-w/2:iy+w/2) ) );
-    %     dt = 1 / norm([vx, vy]);
-    %     next_point = current_point + dt*[vx, vy];
-    %     route = vertcat(route, next_point);
-    % end
-
-    % figure
-    % plot (route(:,1), route(:,2), 'r', 'LineWidth', 2);
-    % grid on
-
-    % *******************************************************************
+    
 end
